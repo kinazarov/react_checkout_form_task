d838b85da9c7066fe677a89ed19ad2c0272ea4a7
kinazarov <kinazarov@gmail.com>
2022-12-02 11:31:41 +0200

    datepicker to restore state

--- src/components/CheckoutPage/Forms/OrderdatesForm.jsx
+++ src/components/CheckoutPage/Forms/OrderdatesForm.jsx
@@ -2,15 +2,34 @@ import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import { SelectDateTime } from '../../FormFields';
 import { useFormikContext } from 'formik';
-import { clone, cloneDeep } from 'lodash';
+import { clone, cloneDeep, find as _find } from 'lodash';
 
 export default function OrderdatesForm(props) {
   const formikContext = useFormikContext();
 
   const [dateTimeState, setDateTimeState] = React.useState(props.datesData);
 
-  const { formField } = props;
-  console.log('◩◩◩◩◩◩ formField start', formField);
+  const findDateTime = (
+    dateTime,
+    desired_date_id,
+    desired_time_id,
+    searchObjects
+  ) => {
+    let dateNode = dateTime
+      ? _find(
+          dateTime,
+          (searchObjects && searchObjects[0]) || ['id', desired_date_id]
+        )
+      : undefined;
+    let timeNode = dateNode
+      ? _find(
+          dateNode.available_hours,
+          (searchObjects && searchObjects[0]) || ['id', desired_time_id]
+        )
+      : undefined;
+
+    return { dateNode, timeNode };
+  };
 
   function cellClick(e) {
     let dateTime = cloneDeep(props.datesData);
@@ -18,31 +37,23 @@ export default function OrderdatesForm(props) {
     const desired_date_id = e.target.getAttribute('desired_date_id');
 
     const desired_time_id = e.target.getAttribute('desired_time_id');
-    console.log('◩◩◩◩◩◩ formikContext.values', formikContext.values);
 
     let newFormikValues = clone(formikContext.values);
 
-    for (let dateRow of dateTime) {
-      let selectedDate = dateRow.id === desired_date_id;
-      dateRow.color = selectedDate ? 'grey' : undefined;
-      console.log('◩◩◩◩◩◩ formikContext', formikContext);
-
-      if (selectedDate) {
-        newFormikValues.desiredDate = dateRow.date;
-        for (let timeCell of dateRow.available_hours) {
-          let selectedTime = timeCell.id === desired_time_id;
-          timeCell.color = selectedTime ? 'cyan' : undefined;
-          if (selectedTime) {
-            newFormikValues.desiredTime = dateRow.date;
-            formikContext.setValues(newFormikValues);
-          }
-        }
-
-        break;
-      }
+    let { dateNode, timeNode } = findDateTime(
+      dateTime,
+      desired_date_id,
+      desired_time_id
+    );
+    if (dateNode) {
+      dateNode.color = 'gray';
+      newFormikValues.desiredDate = dateNode.date;
+    }
+    if (timeNode) {
+      timeNode.color = 'cyan';
+      newFormikValues.desiredTime = dateNode.hours;
     }
 
-    console.log('◩◩◩◩◩◩ formikContext.values', formikContext.values);
     setDateTimeState(dateTime);
   }
 
